8cc41b075839f45ea26c02dd3259741dbe35d906
tianshihao <nd2brth@126.com>
2020-05-30 20:25:44 +0800

    放置按钮

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,11 +2,14 @@
 #define MAINWINDOW_H
 
 #include "grammer.h"
+#include <QButtonGroup>
 #include <QGridLayout>
 #include <QMainWindow>
+#include <QPushButton>
 #include <QTextBlock>
 #include <QTextBrowser>
 #include <QTextEdit>
+#include <QVBoxLayout>
 #include <iostream>
 
 class MainWindow : public QMainWindow
@@ -18,12 +21,18 @@ private:
     QGridLayout *m_gridLayout;
     QTextEdit *m_inputArea;
     QWidget *m_buttonArea;
+    QVBoxLayout *m_buttonLayout;
     QTextBrowser *m_outputArea;
+    QButtonGroup *m_buttonGroup;
+    QPushButton *m_pushButton1;
+    QPushButton *m_pushButton2;
+    QPushButton *m_pushButton3;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
     void ParseText();
 };
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -15,6 +15,24 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     m_buttonArea = new QWidget(this);
     m_buttonArea->setFixedSize(200, 600);
 
+    // 按钮们
+    m_pushButton1 = new QPushButton(m_buttonArea);
+    m_pushButton1->setText((QString) "这是一个按钮");
+
+    m_pushButton2 = new QPushButton(m_buttonArea);
+    m_pushButton2->setText((QString) "这是另外一个按钮");
+
+    m_pushButton3 = new QPushButton(m_buttonArea);
+    m_pushButton3->setText((QString) "这是一个复读按钮");
+
+    // 按钮区的布局
+    m_buttonLayout = new QVBoxLayout(m_buttonArea);
+    m_buttonLayout->addWidget(m_pushButton1);
+    m_buttonLayout->addWidget(m_pushButton2);
+    m_buttonLayout->addWidget(m_pushButton3);
+
+    // m_buttonArea->setLayout(m_buttonLayout);
+
     // 结果输出区
     m_outputArea = new QTextBrowser(this);
     m_outputArea->setText("这里输出结果");
@@ -24,6 +42,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     m_gridLayout->addWidget(m_outputArea, 0, 2);
 
     this->setCentralWidget(m_centralWidget);
+
+    connect(m_pushButton1, SIGNAL(clicked()), this, SLOT(ParseText()));
 }
 
 MainWindow::~MainWindow()
@@ -36,6 +56,8 @@ void MainWindow::ParseText()
     QString inputText = m_inputArea->toPlainText();
 
     Grammer G(inputText.toStdString());
+
+    m_outputArea->setText((QString) "啊啊啊啊");
 
     return;
 }
