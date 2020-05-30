5251676486e95440583b566a85210514b94f14e4
tianshihao <nd2brth@126.com>
2020-05-30 15:09:38 +0800

    主窗口布局完成

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -1,12 +1,23 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QGridLayout>
 #include <QMainWindow>
+#include <QTextBlock>
+#include <QTextBrowser>
+#include <QTextEdit>
 #include <iostream>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+private:
+    QWidget *m_centralWidget;
+    QGridLayout *m_gridLayout;
+    QTextEdit *m_inputArea;
+    QWidget *m_buttonArea;
+    QTextBrowser *m_outputArea;
 
 public:
     MainWindow(QWidget *parent = nullptr);
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,7 +2,28 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    this->setFixedSize(800,600);
+    this->setMinimumSize(800, 600);
+
+    m_centralWidget = new QWidget(this);
+    m_gridLayout = new QGridLayout(m_centralWidget);
+
+    // 文法输入区
+    m_inputArea = new QTextEdit(this);
+    m_inputArea->setText("这里输入文法");
+
+    // 放置按钮的地方
+    m_buttonArea = new QWidget(this);
+    m_buttonArea->setFixedSize(200, 600);
+
+    // 结果输出区
+    m_outputArea = new QTextBrowser(this);
+    m_outputArea->setText("这里输出结果");
+
+    m_gridLayout->addWidget(m_inputArea, 0, 0);
+    m_gridLayout->addWidget(m_buttonArea, 0, 1);
+    m_gridLayout->addWidget(m_outputArea, 0, 2);
+
+    this->setCentralWidget(m_centralWidget);
 }
 
 MainWindow::~MainWindow()
