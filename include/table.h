d79f3df75ae097f06605553410a54482bbe5c89a
tianshihao <tiansh60311@126.com>
2020-06-14 09:10:39 +0800

    feature(table): 新增表格类.

---
+++ include/table.h
@@ -0,0 +1,53 @@
+﻿#ifndef TABLE_H
+#define TABLE_H
+
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+// 表格
+class Table
+{
+private:
+    // 表格行数
+    int m_row;
+    // 表格列数
+    int m_column;
+    // 表格大小
+    int m_size;
+
+    // 表格行头
+    std::map<std::string, int> m_rowHeader;
+    // 表格列头
+    std::map<std::string, int> m_columnHeader;
+    // std::vector<std::tuple<std::string, std::string, std::string>> cells;
+    std::vector<std::string> m_cells;
+
+public:
+    /**
+     *  @brief  初始化分析表
+     *  @param  __VN  行头
+     *  @param  __VT  列头
+     */
+    void InitTable(std::set<std::string> __VN, std::set<std::string> __VT);
+
+    // std::string GetCellData();
+
+    /**
+     *  @brief  将表格的 rowHeader 行, columnHeader 列的单元格内容设置为 content.
+     *  @param  rowHeader  单元格行标
+     *  @param  columnHeader  单元格列标
+     *  @param  content  将要设置的内容
+     */
+    void SetCellData(std::string row, std::string column, std::string content);
+
+    /**
+     *  @brief  打印表格
+     */
+    void PrintTable();
+};
+
+#endif // TABLE_H
---
+++ src/table.cpp
@@ -0,0 +1,75 @@
+﻿#include "table.h"
+
+void Table::InitTable(std::set<std::string> __VN, std::set<std::string> __VT)
+{
+    int value = 0;
+    for (auto symbol : __VN)
+    {
+        m_rowHeader.insert(std::pair<std::string, int>(symbol, value++));
+    }
+
+    m_row = value;
+
+    value = 0;
+    for (auto symbol : __VT)
+    {
+        m_columnHeader.insert(std::pair<std::string, int>(symbol, value++));
+    }
+
+    m_column = value;
+
+    m_size = m_row * m_column;
+
+    for (int i = 0; i < m_size; ++i)
+    {
+        m_cells.push_back(std::string("error"));
+    }
+
+    return;
+}
+
+void Table::SetCellData(std::string row, std::string column, std::string content)
+{
+    m_cells[m_rowHeader[row] * m_column + m_columnHeader[column]] = row + "->" + content;
+}
+
+void Table::PrintTable()
+{
+    int printRow = m_row + 2;
+    int printColumn = m_column + 1;
+    int cellWidth = 12;
+
+    // std::cout << std::fixed << std::left;
+
+    std::cout << std::setw(cellWidth) << std::setfill(' ') << " ";
+    auto itColumnHeader = m_columnHeader.begin();
+
+    // 打印列头
+    for (int i = 0; i < m_column; ++i, itColumnHeader++)
+    {
+        std::cout << "|";
+        std::cout << std::setw(cellWidth) << itColumnHeader->first;
+    }
+
+    std::cout << "\n";
+    std::cout << std::setfill(' ');
+
+    auto itRowHeader = m_rowHeader.begin();
+
+    // 按行打印表格内容
+    for (int i = 0; i < m_row; ++i, itRowHeader++)
+    {
+        std::cout << std::setw(cellWidth) << itRowHeader->first;
+
+        for (int j = 0; j < m_column; ++j)
+        {
+            std::cout << std::setw(cellWidth) << m_cells[i * m_column + j];
+        }
+
+        std::cout << "\n";
+    }
+
+    std::cout << "\n";
+
+    return;
+}
